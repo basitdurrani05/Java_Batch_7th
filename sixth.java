f70b8d6e54a41b20020dcfcf95515c224f4acb63
basitdurrani <basitdurrani05786@gmail.com>
2025-07-05 21:35:21 +0530

    Second Commit

--- sixth.java
+++ sixth.java
@@ -1,16 +1,63 @@
 // String - Sequence Of Character
 
 
-
 public class sixth {
 
 
     public static void main(String[] args) {
 
 
-        String a = "Basit Durrani, and he is a good boy...";
+        // Imp : 
 
-        System.out.println(a);
+        // String a = "Basit Durrani, and he is a good boy...";
+
+        // System.out.println(a);
+
+
+        // Capital Letter
+
+
+        // String a = "kabir khan";
+
+        // System.out.println(a.toUpperCase());
+
+
+        // Small Letter
+
+        // String a = "KABIR KHAN";
+
+        // System.out.println(a.toLowerCase());
+
+
+        // Remove Whitespace
+
+        // String a = "                                         kabir                                           ";
+        // System.out.println(a.trim());
+
+
+        // Replace
+
+        // String a = "Kabir";
+
+        // System.out.println(a.replace("Kabir", "sammer"));
+
+
+
+        // Escape Sequence   - Special type keyword
+
+
+        // System.out.println("Kabir\nKabir");  \n - nextline
+
+
+        // System.out.println("Kabir\tKabir");  \t - space
+
+        // System.out.println("\"Hello World\"");    
+       
+
+
+
+
+        
         
     }
     
