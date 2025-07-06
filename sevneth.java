e511ba55e8d68f9f2811ee572775d2c44df72753
basitdurrani <basitdurrani05786@gmail.com>
2025-07-06 20:35:10 +0530

    Third Commit

---
+++ sevneth.java
@@ -0,0 +1,69 @@
+// Conditionls Statements
+
+import java.util.*;;
+
+public class sevneth {
+
+
+    public static void main(String[] args) {
+
+        // int age = 21;
+
+        // if(age>=18){
+
+        //     System.out.println("Vote...");
+        // }
+
+        // else if(age==0){
+
+        //     System.out.println("Not age.....");
+        // }
+
+        // else{
+
+        //     System.out.println("Not Eligible");
+        // }
+
+
+
+        // This program check your age you are child or teenage or adult
+
+
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter Age : ");
+        // int age = sc.nextInt();   
+
+
+
+        // if(age>=18){
+
+        //     System.out.println("You are a Adult Person.");
+        // }
+
+        // else if(age<0){
+
+        //     System.out.println("This is not a age.....");
+        // }
+
+        // else{
+
+        //     System.out.println("You are a Child.");
+        // }
+
+
+
+        // HomeWork : Q1).Aagar tum 18 number laatey ho toh tume mobile mil jaye or aagar tum nhi laate 18 number toh tumhey kuch bhi nhi miley ?
+
+        // Conditionls
+
+        // marks>=18 : Congrats : Mobile
+        // Aagar nhi laatey : Kuch Nhi
+
+
+
+
+        
+    }
+    
+}
