b86882822373a2d863bdcf5d5bfc32d0bb21aa0a
basitdurrani <basitdurrani05786@gmail.com>
2025-07-09 21:29:34 +0530

    Third Commit

--- eighth.java
+++ eighth.java
@@ -3,12 +3,15 @@
 // 1). For
 // 2). While
 // 3). do-while
-// 4). For-Each
+// 4). For-Each   // Array  
 
 
 public class eighth {
 
     public static void main(String[] args) {
+
+
+        // 1). For 
 
 
         // i++ - +1
@@ -17,7 +20,11 @@ public class eighth {
     //     System.out.println(i);
     //    }
 
-    // For
+    // Task : 
+    
+    // 1).For
+    // 2).While
+    // 3).Do-while
 
     // Q1). Print 100 times your name..
     // Q2). Start - 50 Ending 100
---
+++ ninth.java
@@ -0,0 +1,37 @@
+
+
+
+public class ninth {
+
+    public static void main(String[] args) {
+
+        // 2). While Loop
+
+
+        // int i=1;      // Start Condition
+
+        // while(i<=100){     // Jahan tak chalna hai
+
+        //     System.out.println(i);
+        //     i++;   // Increament Hoga
+        
+        // }
+
+
+
+        // 3). Do-While   (Perfect Loop)  
+
+        // Kam sae kam 1 baar toh chalega chahe condition galat ho
+
+
+        // int i=1;  // Starting Condition
+
+        // do{
+
+        //     System.out.println(i);
+        //     i++; // Increament Hoga
+        // }while(i<=100); // Yahan tak Chalega
+        
+    }
+    
+}
