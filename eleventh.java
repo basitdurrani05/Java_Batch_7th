7985742dc55dd8d75b6991684c3fb08234b98905
basitdurrani <basitdurrani05786@gmail.com>
2025-07-14 21:39:04 +0530

    Sixth Commit

---
+++ eleventh.java
@@ -0,0 +1,51 @@
+// Array  - Collection of similar type of data
+
+//   Type
+
+// 1). One Dimensional / 1D
+// 2). Two Dimensional  / 2D / Multi - Dimensional / 3D
+
+
+public class eleventh {
+
+
+    public static void main(String[] args) {
+
+        // 1). 1D Array
+                   
+        //  Indexing
+
+        // 12 -    0
+        // 13 -    1
+        // 14 -    2
+
+        // int a[] = {12,13,14,15,16,17,18,19,20,21,22,100,200};
+
+        // for(int i=0; i<a.length; i++){
+
+        // System.out.println(a[i]);   // a is varaible of array
+        //                                 // i is a variable of loop
+
+
+        // }
+
+
+        // H.W
+
+        // Q1). Print Array Elements. {1000,2000,3000,4000}
+
+        // Q2). Print Array Elements. {155,166,999,777}
+
+        // Q3). Print Array Elements. {10,200,300,40}
+
+        // Q4). Print Array Elements. {15,66,99,7}
+
+        // Q5). Print Array Elements. {1,20,3,40}
+
+        // Q6). Print Array Elements. {60,23,24,55}
+
+       
+        
+    }
+    
+}
