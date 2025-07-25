70b92210eb9ef0166eb8e79d46d37fd19b88c307
basitdurrani <basitdurrani05786@gmail.com>
2025-07-25 21:03:45 +0530

    Updated

---
+++ fifteen.java
@@ -0,0 +1,46 @@
+// Foreach  - Special type loop
+
+
+public class fifteen {
+
+    public static void main(String[] args) {
+
+
+        // First Example
+
+        // int []a = {12,13,14,15};
+
+        // for(int x : a){
+
+        //     System.out.println(x);
+        // }
+
+
+        // Second Example
+
+        // int []b = {1,2,3,4,5,6,7,8,9,10};
+
+        // for(int i : b){
+
+        //     System.out.println(i);
+        // }
+
+
+        // Third Example
+
+
+        // int []c = {10,20,30,40,50};
+
+        // for(int kabir : c){
+
+        //     System.out.println(kabir);
+        // }
+
+     
+
+
+
+        
+    }
+    
+}
---
+++ sixteen.java
@@ -0,0 +1,61 @@
+// Imp : Recursion  - Method Call Itself is called Recursion.
+
+
+
+public class sixteen {
+
+   public static void first(){
+
+    System.out.println("One");
+
+    second();
+    third();
+    forth();
+
+
+   }
+
+   public static void second(){
+
+    System.out.println("Two");
+   }
+
+   public static void third(){
+
+    System.out.println("Three");
+   }
+
+   public static void forth(){
+
+    System.out.println("Four");
+   }
+
+
+
+    public static void main(String[] args) {
+
+        // It is a Wrong Method
+
+        // first();
+        // second();
+
+        // It is a Correct Method
+
+        // first();
+
+
+        // first();
+
+        
+
+       
+
+
+
+
+
+
+        
+    }
+    
+}
